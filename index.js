39f7473e92445617391057bfa5b0cb80dd10b60c
pyphilia <pyphilia@gmail.com>
2020-01-21 10:00:30 +0100

    build: commit build lib

---
+++ index.js
@@ -0,0 +1,8 @@
+const Decorators = require("./lib/decorators/index.js");
+const Validation = require("./lib/validation/validation.js");
+const Constants = require("./lib/constants.js");
+const DivaServices = require( "./lib/utils");
+const API = require( "./lib/requests");
+const XMLBuilders = require( "./lib/builders/xmlBuilders.js");
+
+module.exports = {Decorators, Validation, Constants, DivaServices, XMLBuilders, API};
--- index.mjs
+++
@@ -1,8 +0,0 @@
-import Decorators from "./lib/decorators/index.js";
-import Validation from "./lib/validation/validation.js";
-import Constants from "./lib/constants.js";
-import DivaServices from "./lib/utils"
-import API from "./lib/requests"
-import XMLBuilders from "./lib/builders/xmlBuilders.js"
-
-export {Decorators, Validation, Constants, DivaServices, XMLBuilders, API};
